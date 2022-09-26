7cf10c3de8a8d9c98f75975e1047a10dd87ba680
creativekashifq <creativekashifq@gmail.com>
2022-09-26 14:49:51 +0500

    fix: database configration updated

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -4,11 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 export const dbConfig = TypeOrmModule.forRootAsync({
     useFactory: () => ({
         type: 'mysql',
-        host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        host: process.env.DB_HOST ? process.env.DB_HOST : 'localhost',
+        port: Number(process.env.DB_PORT) ? Number(process.env.DB_PORT) : 3306,
+        username: process.env.DB_USER ? process.env.DB_USER : 'root',
+        password: process.env.DB_PASSWORD ? process.env.DB_PASSWORD : '',
+        database: process.env.DB_NAME ? process.env.DB_NAME : 'database_name',
         entities: [User, Course],
         synchronize: true,
     })
